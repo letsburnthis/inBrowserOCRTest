9d6b8c7667cd6fdee9b75688ef415c760872bb15
letsburnthis <54116705+letsburnthis@users.noreply.github.com>
2020-11-08 20:02:21 -0500

    basic working example

---
+++ src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+    /* eslint-disable */
+	import  Tesseract  from 'tesseract.js';
+	
+   
+    let imgText = '';
+
+    function recognize(){
+        console.log('function started');
+        const img = document.getElementById('input').files[0];
+        console.log('got selected file');
+        Tesseract.recognize(
+        img,
+        'eng',
+        { logger: m => console.log(m) }
+        ).then(({ data: { text } }) => {
+            console.log(text);
+            imgText=text;
+        })
+        
+
+
+
+    }
+
+
+
+    
+
+
+
+
+
+</script>
+
+
+
+
+<div id="app">
+    <p>Choose an image file on your computer to run inbrowser OCR on. Nothing ever leaves your computer.</p>
+    
+    <!--allows you to select a file on local computer, this file path is then accessed through pure javascript as "input"-->
+    <!--could use a v-on:change to automatically run ocr but keeping them seperate just to show both steps-->
+    <input type="file" id="input">
+    <button on:click={recognize}>recognize</button>
+
+   {#if imgText != ''}
+    <p>The text in the picture you submitted says "{imgText}"</p>
+   {/if} 
+</div>
